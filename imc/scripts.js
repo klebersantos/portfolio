dc2dd12c2de907fd25af1490a38436d9c72179e6
Kleber Santos <j.kleber.s@gmail.com>
2020-07-05 12:53:57 -0300

    imc

---
+++ imc/scripts.js
@@ -0,0 +1,41 @@
+const html = document.querySelector("html")
+const checkbox = document.querySelector("input[name=theme]")
+
+const getStyle = (element, style) => 
+    window
+        .getComputedStyle(element)
+        .getPropertyValue(style)
+
+
+const initialColors = {
+    bg: getStyle(html, "--bg"),
+    bgPanel: getStyle(html, "--bg-panel"),
+    colorHeadings: getStyle(html, "--color-headings"),
+    colorText: getStyle(html, "--color-text"),
+    bgCenter: getStyle(html, "--bg-center"),
+    bgSombra: getStyle(html, "--bg-sombra"),
+}
+
+const darkMode = {
+    bg: "#333333",
+    bgPanel: "#434343",
+    colorHeadings: "#fff",
+    colorText: "#B5B5B5",
+    bgCenter: "#000",
+    bgSombra: "#000"
+}
+
+const transformKey = key => 
+    "--" + key.replace(/([A-Z])/, "-$1").toLowerCase()
+
+
+const changeColors = (colors) => {
+    Object.keys(colors).map(key => 
+        html.style.setProperty(transformKey(key), colors[key]) 
+    )
+}
+
+
+checkbox.addEventListener("change", ({target}) => {
+    target.checked ? changeColors(darkMode) : changeColors(initialColors)
+})
